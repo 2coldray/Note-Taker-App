1e894a18eebb9599076b0f34e04d8e879736a094
Ray Bray <rbray.bray2@gmail.com>
2020-10-03 00:35:03 -0400

    added comments, ready to test and then deploy app

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+//=========================================================
 //Get Routes
 
 //Html home
